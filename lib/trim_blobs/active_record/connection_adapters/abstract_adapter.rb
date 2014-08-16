012f07efc03f6644ada3531ad27d44daa88f7ffe
Jarl Friis <jarl@softace.dk>
2014-08-16 09:02:23 +0200

    Support for Rails 4.0 and 4.1

--- lib/trim_blobs/active_record/connection_adapters/abstract_adapter.rb
+++ lib/trim_blobs/active_record/connection_adapters/abstract_adapter.rb
@@ -2,13 +2,17 @@ module TrimBlobs
   module ActiveRecord
     module ConnectionAdapters
       module AbstractAdapter
-        def log_with_blobs_trimmed(sql, name = "SQL", binds = [])
+        def log_with_blobs_trimmed(sql, name = "SQL", binds = [], statement_name = nil)
           if sql
             sql = sql.gsub(/'\\x((?:[0-9a-f]{2})+)'/) do |blob|
               (blob.size > 32) ? "'\\x#{$1[0,32]}... (TRIMMED #{blob.size} hexadecimal digits)'" : $&
             end
           end
-          log_without_blobs_trimmed(sql, name, binds){ yield }
+          if statement_name
+            log_without_blobs_trimmed(sql, name, binds, statement_name){ yield }
+          else
+            log_without_blobs_trimmed(sql, name, binds){ yield }
+          end
         end
       end
     end
